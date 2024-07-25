2a4722024b2985e6b05026e34b6bbd1ca85b5e51
Vincent <vafdaf11@gmail.com>
2024-07-25 21:34:23 +0200

    Initial commit

---
+++ src/main/java/odb/gradle/App.java
@@ -0,0 +1,36 @@
+package odb.gradle;
+
+import javax.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args) {
+        var emf = Persistence.createEntityManagerFactory("objectdb:db/points.odb");
+        var em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        for (int i = 0; i < 20; i++) {
+            var p = new Point(i, i);
+            em.persist(p);
+        }
+        em.getTransaction().commit();
+
+        // Find the number of Point objects in the database:
+        var q1 = em.createQuery("SELECT COUNT(p) FROM Point p");
+        System.out.println("Total Points: " + q1.getSingleResult());
+
+        // Find the average X value:
+        var q2 = em.createQuery("SELECT AVG(p.x) FROM Point p");
+        System.out.println("Average X: " + q2.getSingleResult());
+
+        // Retrieve all the Point objects from the database:
+        var query = em.createQuery("SELECT p FROM Point p", Point.class);
+        var results = query.getResultList();
+        for (Point p : results) {
+            System.out.println(p);
+        }
+
+        // Close the database connection:
+        em.close();
+        emf.close();
+    }
+}
